7e058facfc3a046131dc965382e690212ea1014e
Carlos Mougan <carmougan@gmail.com>
2022-03-15 09:00:58 +0100

    ood data

--- synthetic/gaussianShiftrandom.py
+++ synthetic/gaussianShiftrandom.py
@@ -12,14 +12,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression, Lasso
 
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 import shap
 
 sys.path.append("../")
 from fairtools.xaiUtils import ShapEstimator
 
-# %%
-np.random.random(4)
 # %%
 ## Create variables
 ### Normal
@@ -54,10 +52,10 @@ df["target"] = df["Var1"] * df["Var2"]
 ## Fit our ML model
 X_tr, X_te, y_tr, y_te = train_test_split(df.drop(columns="target"), df[["target"]])
 # %%
-model = GradientBoostingRegressor()
+model = RandomForestRegressor()
 preds_val = cross_val_predict(model, X_tr, y_tr, cv=3)
 model.fit(X_tr, y_tr)
-y_hat = model.predict(X_te)
+preds_test = model.predict(X_te)
 # %%
 ## Real explanation
 explainer = shap.Explainer(model)
@@ -66,28 +64,31 @@ exp = pd.DataFrame(
     data=shap_values.values, columns=["Shap%d" % (i + 1) for i in range(3)]
 )
 # %%
-## Fake explanation
-fake = pd.DataFrame(data=[x11, x22, x33]).T
-y_hat1 = model.predict(fake)
-shap_values = explainer(fake)
-exp_fake = pd.DataFrame(
-    data=fake.values, columns=["Shap%d" % (i + 1) for i in range(3)]
+## OOD Data + OODexplanation
+X_ood = pd.DataFrame(data=[x11, x22, x33]).T
+X_ood.columns = ["Var%d" % (i + 1) for i in range(X_ood.shape[1])]
+preds_ood = model.predict(X_ood)
+shap_values = explainer(X_ood)
+exp_ood = pd.DataFrame(
+    data=X_ood.values, columns=["Shap%d" % (i + 1) for i in range(3)]
 )
+y_ood = X_ood["Var1"] * X_ood["Var2"]
 # %%
 print("Feat 1")
-print(ks_2samp(exp_fake["Shap1"], exp["Shap1"]))
+print(ks_2samp(exp_ood["Shap1"], exp["Shap1"]))
 print(ks_2samp(x11, x1))
 # %%
 print("Feat 2")
-print(ks_2samp(exp_fake["Shap2"], exp["Shap2"]))
+print(ks_2samp(exp_ood["Shap2"], exp["Shap2"]))
 print(ks_2samp(x22, x2))
 # %%
 print("Feat 3")
-print(ks_2samp(exp_fake["Shap3"], exp["Shap3"]))
+print(ks_2samp(exp_ood["Shap3"], exp["Shap3"]))
 print(ks_2samp(x33, x3))
 # %%
 print("Target")
-print(ks_2samp(y_hat, y_hat1))
+print(ks_2samp(y_te["target"], y_ood))
+print(ks_2samp(preds_test, preds_ood))
 
 # %%
 ## Does xAI help to solve this issue?
@@ -98,31 +99,44 @@ shap_pred_tr = pd.DataFrame(shap_pred_tr, columns=X_tr.columns)
 shap_pred_tr = shap_pred_tr.add_suffix("_shap")
 
 se.fit(X_tr, y_tr)
-
 # %%
-clf = Pipeline([("scaler", StandardScaler()), ("svc", Lasso())])
-# clf = RandomForestClassifier()
+# clf = Pipeline([("scaler", StandardScaler()), ("svc", Lasso())])
+clf = XGBRegressor()
 error_tr = y_tr.target.values - preds_val
 preds_tr_shap = cross_val_predict(clf, shap_pred_tr, error_tr, cv=3)
 clf.fit(shap_pred_tr, error_tr)
 
 # %%
+## Test Set
 shap_pred_te = se.predict(X_te)
 shap_pred_te = pd.DataFrame(shap_pred_te, columns=X_te.columns)
 shap_pred_te = shap_pred_te.add_suffix("_shap")
-error_te = y_te.target.values - y_hat
+
+error_te = y_te.target.values - preds_test
 preds_te_shap = clf.predict(shap_pred_te)
+
+## OOD Set
+shap_pred_ood = se.predict(X_ood)
+shap_pred_ood = pd.DataFrame(shap_pred_ood, columns=X_te.columns)
+shap_pred_ood = shap_pred_ood.add_suffix("_shap")
+
+error_ood = y_ood - preds_ood
+preds_ood_shap = clf.predict(shap_pred_ood)
+
 # %%
 ## Only SHAP
 print(mean_squared_error(error_tr, preds_tr_shap))
 print(mean_squared_error(error_te, preds_te_shap))
+print(mean_squared_error(error_ood, preds_ood_shap))
 # %%
 ## Only data
 preds_tr_shap = cross_val_predict(clf, X_tr, error_tr, cv=3)
 clf.fit(X_tr, error_tr)
 preds_te_shap = clf.predict(X_te)
+preds_ood_shap = clf.predict(X_ood)
 print(mean_squared_error(error_tr, preds_tr_shap))
 print(mean_squared_error(error_te, preds_te_shap))
+print(mean_squared_error(error_ood, preds_ood_shap))
 
 # %%
 ## SHAP + Data
@@ -132,11 +146,24 @@ tr_full = pd.concat(
 te_full = pd.concat(
     [shap_pred_te.reset_index(drop=True), X_te.reset_index(drop=True)], axis=1
 )
+ood_full = pd.concat(
+    [shap_pred_ood.reset_index(drop=True), X_ood.reset_index(drop=True)], axis=1
+)
 
 preds_tr_shap = cross_val_predict(clf, tr_full, error_tr, cv=3)
 clf.fit(tr_full, error_tr)
 preds_te_shap = clf.predict(te_full)
+preds_ood_shap = clf.predict(ood_full)
 print(mean_squared_error(error_tr, preds_tr_shap))
 print(mean_squared_error(error_te, preds_te_shap))
+print(mean_squared_error(error_ood, preds_ood_shap))
+
+# %%
+# Original error
+print(mean_squared_error(y_tr, preds_val))
+print(mean_squared_error(y_te, preds_test))
+print(mean_squared_error(y_ood, preds_ood))
+
+# %%
 
 # %%
