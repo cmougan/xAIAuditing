b348ae8e0531e8eaa2340acd7ef3a1df3502feae
Carlos Mougan <carmougan@gmail.com>
2022-03-16 13:05:35 +0100

    estimator loop

--- folks/states.py
+++ folks/states.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import roc_auc_score
 import pandas as pd
+from collections import defaultdict
 from xgboost import XGBRegressor, XGBClassifier
 from scipy.stats import kstest
 import shap
@@ -12,6 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 import sys
 
@@ -82,55 +84,66 @@ for feat in ca_features.columns:
     else:
         print(feat, " is equivalent ", pval)
 # %%
-## Shap Estimator
+## Shap Estimator on CA and MI
 se = ShapEstimator(model=XGBRegressor())
 shap_pred_ca = cross_val_predict(se, ca_features, ca_labels, cv=3)
 shap_pred_ca = pd.DataFrame(shap_pred_ca, columns=ca_features.columns)
 shap_pred_ca = shap_pred_ca.add_suffix("_shap")
 
 se.fit(ca_features, ca_labels)
-# %%
-# clf = Pipeline([('scaler', StandardScaler()), ('svc', LogisticRegression())])
-clf = RandomForestClassifier()
 error_ca = ca_labels == preds_ca
-preds_ca_shap = cross_val_predict(
-    clf, shap_pred_ca, error_ca, cv=3, method="predict_proba"
-)[:, 1]
-clf.fit(shap_pred_ca, error_ca)
+# %%
+# Estimators for the loop
+estimators = defaultdict()
+estimators["Linear"] = Pipeline(
+    [("scaler", StandardScaler()), ("model", LogisticRegression())]
+)
+estimators["RandomForest"] = RandomForestClassifier(random_state=0)
+estimators["XGBoost"] = XGBClassifier(random_state=0)
+estimators["MLP"] = MLPClassifier(random_state=0)
+# %%
+# Loop over different G estimators
+for estimator in estimators:
+    print(estimator)
+    clf = estimators[estimator]
+
+    preds_ca_shap = cross_val_predict(
+        clf, shap_pred_ca, error_ca, cv=3, method="predict_proba"
+    )[:, 1]
+    clf.fit(shap_pred_ca, error_ca)
+
+    shap_pred_mi = se.predict(mi_features)
+    shap_pred_mi = pd.DataFrame(shap_pred_mi, columns=ca_features.columns)
+    shap_pred_mi = shap_pred_mi.add_suffix("_shap")
+    error_mi = mi_labels == preds_mi
+    preds_mi_shap = clf.predict_proba(shap_pred_mi)[:, 1]
+
+    ## Only SHAP
+    print("Only Shap")
+    print(roc_auc_score(error_ca, preds_ca_shap))
+    print(roc_auc_score(error_mi, preds_mi_shap))
+    ## Only data
+    print("Only Data")
+    preds_ca_shap = cross_val_predict(
+        clf, ca_features, error_ca, cv=3, method="predict_proba"
+    )[:, 1]
+    clf.fit(ca_features, error_ca)
+    preds_mi_shap = clf.predict_proba(mi_features)[:, 1]
+    print(roc_auc_score(error_ca, preds_ca_shap))
+    print(roc_auc_score(error_mi, preds_mi_shap))
+
+    ## SHAP + Data
+    print("Shap + Data")
+    ca_full = pd.concat([shap_pred_ca, ca_features], axis=1)
+    mi_full = pd.concat([shap_pred_mi, mi_features], axis=1)
+
+    preds_ca_shap = cross_val_predict(
+        clf, ca_full, error_ca, cv=3, method="predict_proba"
+    )[:, 1]
+    clf.fit(ca_full, error_ca)
+    preds_mi_shap = clf.predict_proba(mi_full)[:, 1]
+    print(roc_auc_score(error_ca, preds_ca_shap))
+    print(roc_auc_score(error_mi, preds_mi_shap))
 
 # %%
-shap_pred_mi = se.predict(mi_features)
-shap_pred_mi = pd.DataFrame(shap_pred_mi, columns=ca_features.columns)
-shap_pred_mi = shap_pred_mi.add_suffix("_shap")
-error_mi = mi_labels == preds_mi
-preds_mi_shap = clf.predict_proba(shap_pred_mi)[:, 1]
-# %%
-## Only SHAP
-print(roc_auc_score(error_ca, preds_ca_shap))
-print(roc_auc_score(error_mi, preds_mi_shap))
-# %%
-## Only data
-preds_ca_shap = cross_val_predict(
-    clf, ca_features, error_ca, cv=3, method="predict_proba"
-)[:, 1]
-clf.fit(ca_features, error_ca)
-preds_mi_shap = clf.predict_proba(mi_features)[:, 1]
-print(roc_auc_score(error_ca, preds_ca_shap))
-print(roc_auc_score(error_mi, preds_mi_shap))
-
-# %%
-## SHAP + Data
-ca_full = pd.concat([shap_pred_ca, ca_features], axis=1)
-mi_full = pd.concat([shap_pred_mi, mi_features], axis=1)
-
-preds_ca_shap = cross_val_predict(clf, ca_full, error_ca, cv=3, method="predict_proba")[
-    :, 1
-]
-clf.fit(ca_full, error_ca)
-preds_mi_shap = clf.predict_proba(mi_full)[:, 1]
-print(roc_auc_score(error_ca, preds_ca_shap))
-print(roc_auc_score(error_mi, preds_mi_shap))
-
-# %%
-
-# %%
+# Original Error
