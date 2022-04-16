f5e0380b55f7b79e0d651ca0ccc061ac27882988
Carlos Mougan <carmougan@gmail.com>
2022-04-16 09:00:19 +0200

    save clean fairness

--- folks/results/cleanFairness.py
+++ folks/results/cleanFairness.py
@@ -1,4 +1,5 @@
 # %%
+from sre_parse import State
 import pandas as pd
 import os
 import warnings
@@ -15,11 +16,31 @@ for file in files:
 # data.to_csv("all_results.csv", index=False)
 
 # %%
-data
 # %%
-file
-# %%
-data[(data["state"] == "MI") & (data["error_type"] == "fairness_one")]
-# %%
+df = pd.DataFrame()
+for STATE in df.state.unique():
+    aux = data[(data["state"] == STATE) & (data["error_type"] == "fairness_one")]
+    y2_ood = data[
+        (data["state"] == STATE) & (data["error_type"] == "fairness_two")
+    ].error_ood
+    y2_te = data[(data["state"] == STATE) & (data["error_type"] == "fairness_two")].error_te
 
+    aux["y2_te"] = y2_te
+    aux["y2_ood"] = y2_ood
+    # %%
+    aux.columns = [
+        "state",
+        "error_type",
+        "estimator",
+        "data",
+        "y1_te",
+        "y1_ood",
+        "y2_te",
+        "y2_ood",
+    ]
+    # %%
+    aux['fair_te'] = aux['y1_te'] - aux['y2_te']
+    aux['fair_ood'] = aux['y1_ood'] - aux['y2_ood']
+    df.append(aux)
 # %%
+df.to_csv("all_results_fairness.csv", index=False)
