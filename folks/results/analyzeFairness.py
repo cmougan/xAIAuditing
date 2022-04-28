087e706420478e87d287cc8948e900eae78ad360
Carlos Mougan <carmougan@gmail.com>
2022-04-28 09:29:21 +0200

    acs travel time

--- folks/results/analyzeFairness.py
+++ folks/results/analyzeFairness.py
@@ -3,18 +3,22 @@ import pandas as pd
 import numpy as np
 
 # %%
-df = pd.read_csv("all_results_fairness.csv")
+df = pd.read_csv("all_results_fairness_employment.csv")
 
 df.data = df.data.replace("Only Data", "Data")
 df.data = df.data.replace("Only Shap", "Shap")
 df.data = df.data.replace("Data + Shap", "Shap + Data")
 
 df = df[df["error_type"] == "fairness_two"]
+# dff = df[df["error_type"] == "fairness_one"]
+# df = df[df["error_type"] == "performance"]
 df = df.drop(columns="error_te")
 # df = df[df['state']=='WA']
 df = df[
     (df["estimator"] == "Linear")
     | (df["estimator"] == "Dummy")
+    # | (df["estimator"] == "RandomForest")
+    # | (df["estimator"] == "SVM")
     | (df["estimator"] == "XGBoost")
 ]
 
@@ -35,5 +39,7 @@ pd.pivot_table(
     decimals=6
 ).style.highlight_min()
 # .to_csv("total_mean_fairness.csv")#.style.highlight_min()
+
+# %%
 
 # %%
