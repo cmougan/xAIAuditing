658e2b32f903bc0009618a7eb66c1f5289653588
Carlos Mougan <carmougan@gmail.com>
2022-05-17 15:57:08 +0200

    update input data names

--- folks/results/plotStates.py
+++ folks/results/plotStates.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 
 from pyparsing import col
 
-df = pd.read_csv("all_results.csv")
+df = pd.read_csv("folks/results/all_results_income.csv")
 # %%
 aux = df[df["error_type"] == "performance"]
 aux = aux[aux["estimator"] == "Linear"]
@@ -22,8 +22,8 @@ fig = px.choropleth(
     hover_data=["error_ood"],
 )
 fig.show()
-fig.write_image("../../images/performanceUS.svg", format="svg")
-fig.write_image("../../images/performanceUS.png")
+fig.write_image("images/performanceUS.svg", format="svg")
+fig.write_image("images/performanceUS.png")
 # %%
 aux = df[df["error_type"] == "fairness"]
 aux = aux[(aux["estimator"] == "Linear") & (aux["estimator"] == "XGBoost")]
@@ -50,12 +50,15 @@ for state in aux["state"].unique():
     aux_state = aux[(aux["state"] == state) & (aux["estimator"] == "Linear")]
     # Estimators
     data = aux_state[aux_state["data"] == "Only Data"].error_ood.values
+    target = aux_state[aux_state["data"] == "Only Target"].error_ood.values
+    dataTarget = aux_state[aux_state["data"] == "Data+Target"].error_ood.values
     shap = aux_state[aux_state["data"] == "Only Shap"].error_ood.values
-    both = aux_state[aux_state["data"] == "Data + Shap"].error_ood.values
+    all = aux_state[aux_state["data"] == "Data+Target+Shap"].error_ood.values
+
     # Dummy
     aux_state = aux[(aux["state"] == state) & (aux["estimator"] == "Dummy")]
     dummy = aux_state.error_ood.mean()
-    d = {"data": data, "shap": shap, "both": both, "dummy": dummy}
+    d = {"Distribution Shift": data,'Prediction Shift':target,'Dist+Pred Shift':dataTarget, "Explanation Shift": shap, "Dist+Targ+Exp Shift": all, "dummy": dummy}
 
     best.append([state, min(d, key=d.get)])
 
@@ -72,6 +75,6 @@ fig = px.choropleth(
     # hover_data=["error_ood"],
 )
 fig.show()
-fig.write_image("../../images/best_method.png")
+fig.write_image("images/best_method.png")
 
 # %%
