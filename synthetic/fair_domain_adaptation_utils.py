fb4ca1197b5741d3399481cf1ee907581e1758ae
Carlos Mougan <carmougan@gmail.com>
2022-05-25 18:11:16 +0200

    remove loops

--- synthetic/fair_domain_adaptation_utils.py
+++ synthetic/fair_domain_adaptation_utils.py
@@ -59,73 +59,70 @@ def gen_synth_shift_data(
 
     # Source datasets
     data_src = []
-    for gamma_shift in gamma_shift_src:
-        C_src_vec = np.repeat(a=C_src, repeats=N)
-        A = random_logit(
-            scale_A
-            * (
-                gamma_A
-                + gamma_shift * gamma_AC * C_src_vec
-                + np.random.normal(loc=0.0, scale=scale_e, size=N)
-            )
+    C_src_vec = np.repeat(a=C_src, repeats=N)
+    A = random_logit(
+        scale_A
+        * (
+            gamma_A
+            + gamma_shift_src * gamma_AC * C_src_vec
+            + np.random.normal(loc=0.0, scale=scale_e, size=N)
         )
-        R = gamma_RA * A + np.random.normal(
-            loc=0.0, scale=1 + scale_e, size=N
-        )  # N(0,1)
-        Y_src = random_logit(
-            scale_Y
-            * (
-                gamma_YA * A
-                + gamma_YR * R
-                + np.random.normal(loc=0.0, scale=scale_e, size=N)
-            )
+    )
+    R = gamma_RA * A + np.random.normal(loc=0.0, scale=1 + scale_e, size=N)  # N(0,1)
+    Y_src = random_logit(
+        scale_Y
+        * (
+            gamma_YA * A
+            + gamma_YR * R
+            + np.random.normal(loc=0.0, scale=scale_e, size=N)
         )
-        T = (
-            gamma_TY * Y_src
-            + gamma_TR * R
-            + gamma_TA * A
-            + np.random.normal(
-                loc=0.0, scale=scale_T + gamma_shift * gamma_TC * C_src_vec, size=N
-            )
+    )
+    T = (
+        gamma_TY * Y_src
+        + gamma_TR * R
+        + gamma_TA * A
+        + np.random.normal(
+            loc=0.0, scale=scale_T + gamma_shift_src * gamma_TC * C_src_vec, size=N
         )
+    )
 
-        Y_src = (Y_src + 1) / 2
-        X_src = np.stack([A, R, T], axis=1)
-        data_src.append((gamma_shift, X_src, Y_src))
+    Y_src = (Y_src + 1) / 2
+    X_src = np.stack([A, R, T], axis=1)
+    data_src.append((gamma_shift_src, X_src, Y_src))
 
     # Target datasets
     data_tar = []
-    for gamma_shift in gamma_shift_tar:
-        C_tar_vec = np.repeat(a=C_tar, repeats=N)
-        A = random_logit(
-            scale_A
-            * (
-                gamma_A
-                + gamma_shift * gamma_AC * C_tar_vec
-                + np.random.normal(loc=0.0, scale=scale_e, size=N)
-            )
-        )
-        R = gamma_RA * A + np.random.normal(loc=0.0, scale=1 + scale_e, size=N)
-        Y_tar = random_logit(
-            scale_Y
-            * (
-                gamma_YA * A
-                + gamma_YR * R
-                + np.random.normal(loc=0.0, scale=scale_e, size=N)
-            )
-        )
-        T = (
-            gamma_TY * Y_tar
-            + gamma_TR * R
-            + gamma_TA * A
-            + np.random.normal(
-                loc=0.0, scale=scale_T + gamma_shift * gamma_TC * C_tar_vec, size=N
-            )
-        )
 
-        Y_tar = (Y_tar + 1) / 2
-        X_tar = np.stack([A, R, T], axis=1)
-        data_tar.append((gamma_shift, X_tar, Y_tar))
+    C_tar_vec = np.repeat(a=C_tar, repeats=N)
+    A = random_logit(
+        scale_A
+        * (
+            gamma_A
+            + gamma_shift_src * gamma_AC * C_tar_vec
+            + np.random.normal(loc=0.0, scale=scale_e, size=N)
+        )
+    )
+    R = gamma_RA * A + np.random.normal(loc=0.0, scale=1 + scale_e, size=N)
+    Y_tar = random_logit(
+        scale_Y
+        * (
+            gamma_YA * A
+            + gamma_YR * R
+            + np.random.normal(loc=0.0, scale=scale_e, size=N)
+        )
+    )
+    T = (
+        gamma_TY * Y_tar
+        + gamma_TR * R
+        + gamma_TA * A
+        + np.random.normal(
+            loc=0.0, scale=scale_T + gamma_shift_src * gamma_TC * C_tar_vec, size=N
+        )
+    )
+
+    Y_tar = (Y_tar + 1) / 2
+    X_tar = np.stack([A, R, T], axis=1)
+    data_tar.append((gamma_shift_src, X_tar, Y_tar))
 
     sensible_feature = 0  # A
     non_separating_feature = 2  # T
