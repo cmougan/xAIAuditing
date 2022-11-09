6316f3cffc337880ee48a66e5ab76fe0c53c6775
Carlos Mougan <carmougan@gmail.com>
2022-11-09 09:56:15 +0100

    audit detector class with test

--- fairtools/detector.py
+++ fairtools/detector.py
@@ -1,41 +1,181 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 import shap
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
-from sklearn.metrics import roc_auc_score
 
 
-def shap_detector(data1, data2, verbosity=False):
+class ExplanationAudit(BaseEstimator, ClassifierMixin):
+    """ """
 
-    shapX1 = pd.DataFrame(data1)
-    shapX1.columns = ["var%d" % (i + 1) for i in range(shapX1.shape[1])]
-    shapX1["target"] = 0
-    shapX2 = pd.DataFrame(data2)
-    shapX2.columns = ["var%d" % (i + 1) for i in range(shapX2.shape[1])]
-    shapX2["target"] = 1
-    shapALL = pd.concat([shapX1, shapX2], axis=0)
-    print(shapALL.shape)
-    X_train, X_test, y_train, y_test = train_test_split(
-        shapALL.drop(columns=["target"]),
-        shapALL.target,
-        test_size=0.33,
-        random_state=42,
-    )
-    det = XGBClassifier(
-        n_estimators=100,
-        max_depth=3,
-        learning_rate=0.1,
-        verbosity=0,
-        use_label_encoder=False,
-    )
-    det.fit(X_train.values, y_train.values)
+    def __init__(self, model, gmodel):
+        self.model = model
+        self.gmodel = gmodel
+        self.explainer = None
 
-    if verbosity:
-        print(
-            "AUC Train:",
-            roc_auc_score(y_train, det.predict_proba(X_train.values)[:, 1]),
+        # Supported F Models
+        self.supported_tree_models = ["XGBClassifier", "XGBRegressor"]
+        self.supported_linear_models = [
+            "LogisticRegression",
+            "LinearRegression",
+            "Ridge",
+            "Lasso",
+        ]
+        self.supported_models = (
+            self.supported_tree_models + self.supported_linear_models
         )
-        print(
-            "AUC Test:", roc_auc_score(y_test, det.predict_proba(X_test.values)[:, 1])
+        # Supported detectors
+        self.supported_linear_detectors = [
+            "LogisticRegression",
+        ]
+        self.supported_tree_detectors = ["XGBClassifier"]
+        self.supported_detectors = (
+            self.supported_linear_detectors + self.supported_tree_detectors
         )
-    return roc_auc_score(y_test, det.predict_proba(X_test.values)[:, 1])
+
+        # Check if models are supported
+        if self.get_model_type() not in self.supported_models:
+            raise ValueError(
+                "Model not supported. Supported models are: {} got {}".format(
+                    self.supported_models, self.model.__class__.__name__
+                )
+            )
+        if self.get_gmodel_type() not in self.supported_detectors:
+            raise ValueError(
+                "gmodel not supported. Supported models are: {} got {}".format(
+                    self.supported_detectors, self.gmodel.__class__.__name__
+                )
+            )
+
+    def get_gmodel_type(self):
+        if self.gmodel.__class__.__name__ == "Pipeline":
+            return self.gmodel.steps[-1][1].__class__.__name__
+        else:
+            return self.gmodel.__class__.__name__
+
+    def get_model_type(self):
+        if self.model.__class__.__name__ == "Pipeline":
+            return self.model.steps[-1][1].__class__.__name__
+        else:
+            return self.model.__class__.__name__
+
+    def split_data(self, X, y, n1=0.6, n2=0.5):
+        self.X_tr, X_val, self.y_tr, y_val = train_test_split(
+            X, y, random_state=0, test_size=0.6
+        )
+        self.X_val, self.X_te, self.y_val, self.y_te = train_test_split(
+            X_val, y_val, random_state=0, test_size=0.5
+        )
+
+    def fit(self, X, y, Z):
+
+        # Check that X and y have correct shape
+        check_X_y(X, y)
+        self.Z = Z
+
+        # Split data intro train, validation and test
+        self.split_data(X, y)
+
+        # Extract prottected att. and remove from data
+        self.Z_tr = self.X_tr[self.Z]
+        self.Z_val = self.X_val[self.Z]
+        self.Z_te = self.X_te[self.Z]
+        self.X_tr = self.X_tr.drop(self.Z, axis=1)
+        self.X_val = self.X_val.drop(self.Z, axis=1)
+        self.X_te = self.X_te.drop(self.Z, axis=1)
+
+        # Fit model F
+        self.fit_model(self.X_tr, self.y_tr)
+
+        # Get explanations
+        self.S_val = self.get_explanations(self.X_val)
+
+        # Fir model G
+        self.fit_audit_detector(self.S_val, self.Z_val)
+
+        return self
+
+    def predict(self, X):
+        if self.Z in X.columns:
+            X = X.drop(self.Z, axis=1)
+        return self.gmodel.predict(self.get_explanations(X))
+
+    def predict_proba(self, X):
+        if self.Z in X.columns:
+            X = X.drop(self.Z, axis=1)
+        return self.gmodel.predict_proba(self.get_explanations(X))
+
+    def explanation_predict(self, X):
+        return self.gmodel.predict(X)
+
+    def explanation_predict_proba(self, X):
+        return self.gmodel.predict_proba(X)
+
+    def fit_model(self, X, y):
+        self.model.fit(X, y)
+
+    def fit_audit_detector(self, X, y):
+        self.gmodel.fit(X, y)
+
+    def get_explanations(self, X):
+        # Determine the type of SHAP explainer to use
+        if self.get_model_type() in self.supported_tree_models:
+            self.explainer = shap.Explainer(self.model)
+        elif self.get_model_type() in self.supported_linear_models:
+            self.explainer = shap.LinearExplainer(
+                self.model, X, feature_dependence="correlation_dependent"
+            )
+        else:
+            raise ValueError(
+                "Model not supported. Supported models are: {}, got {}".format(
+                    self.supported_models, self.model.__class__.__name__
+                )
+            )
+
+        shap_values = self.explainer(X)
+        # Name columns
+        if isinstance(X, pd.DataFrame):
+            columns_name = X.columns
+        else:
+            columns_name = ["Shap%d" % (i + 1) for i in range(X.shape[1])]
+
+        exp = pd.DataFrame(
+            data=shap_values.values,
+            columns=columns_name,
+        )
+        return exp
+
+    def get_auc_val(self):
+        """
+        Returns the AUC of the validation set
+
+        """
+        return roc_auc_score(self.y_te, self.predict_proba(self.X_te)[:, 1])
+
+    def get_coefs(self):
+        if self.gmodel.__class__.__name__ == "Pipeline":
+            if (
+                self.gmodel.steps[-1][1].__class__.__name__
+                in self.supported_linear_models
+            ):
+                return self.gmodel.steps[-1][1].coef_
+            else:
+                raise ValueError(
+                    "Pipeline model not supported. Supported models are: {}, got {}".format(
+                        self.supported_linear_models,
+                        self.gmodel.steps[-1][1].__class__.__name__,
+                    )
+                )
+        else:
+            return self.get_linear_coefs()
+
+    def get_linear_coefs(self):
+        if self.gmodel.__class__.__name__ in self.supported_linear_models:
+            return self.gmodel.coef_
+        else:
+            raise ValueError(
+                "Detector model not supported. Supported models ar linear: {}, got {}".format(
+                    self.supported_linear_detector, self.model.__class__.__name__
+                )
+            )
